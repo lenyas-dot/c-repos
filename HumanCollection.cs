00f40660a89eae92fa246c29eba0905bcb97dcce
Lyonya <lenya.luzarenko@gmail.com>
2020-09-28 20:45:41 +1000

    Delegate Training

---
+++ HumanCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    delegate bool IsFind<TItem>(TItem item);
+
+    class HumanCollection<THuman>
+    {
+        private List<THuman> _humans;
+
+        public HumanCollection()
+        {
+            _humans = new List<THuman>();
+        }
+
+        public void Add(THuman human)
+        {
+            _humans.Add(human);
+        }
+
+        public bool Exist(IsFind<THuman> isFind)
+        {
+            foreach (THuman item in _humans)
+            {
+                bool compare = isFind.Invoke(item);
+                if (compare)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HumanCollection<User> collection = new HumanCollection<User>();
+            collection.Add(new User()
+            {
+                Id = 1,
+                FullName = "Иван"
+            });
+            collection.Add(new User()
+            {
+                Id = 2,
+                FullName = "Антон"
+            });
+
+            bool antonExist = collection.Exist(x => x.FullName == "Антон");
+
+            bool idExist = collection.Exist(x => x.Id == 2);
+
+        }
+
+      
+    }
+}
